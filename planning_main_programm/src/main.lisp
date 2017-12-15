40727ad941eaa2e7e1e49542424362b4d9f8355f
Unknown <stoermer@uni-bremen.de>
2017-12-15 17:21:34 +0100

    Main verfeinert + rosinfo
    Main um ein bisschen was verfeinert und jede Menge rosinfos eingefügt

--- planning_main_programm/src/main.lisp
+++ planning_main_programm/src/main.lisp
@@ -11,7 +11,9 @@
   "Main function - Executing and planning robot behaviour on the top level"
   (init-variables)
   (roslisp:with-ros-node ("planning_main")
+    (roslisp::ros-info "Main" "Robotlife seems hard, but lets do this")
     (planning-move::move-Head *x* *y* *z*)
+    (roslisp::ros-info "Main" "Moving into homeposition")
     (planning-motion::call-Motion-Move-Arm)
     (let ((counter 10))
       (block check-for-valid-point 
@@ -19,15 +21,20 @@
               do (progn
                    (let ((point-for-motion
                             (let ((point-center-of-object
-                                    (planning-vision::call-vision-point)))
+                                    (progn
+                                      (roslisp::ros-info "Main" "Calling Vision for center point of object")
+                                      (planning-vision::call-vision-point)))
                               (progn
                                 (setf *point-center-of-object* point-center-of-object)
+                                (roslisp:ros-info "Main" "Asking knowledge where to poke")
                                 (planning-knowledge::ask-knowledge point-center-of-object)))))
                       (if (planning-vision::check-points-is-equal (planning-vision::call-vision-point) *point-center-of-object* 0.2)
-                          (progn (planning-motion::call-Motion-Move-To-Point point-for-motion)
-                                 (return-from check-for-valid-point))
+                          (progn
+                            (roslisp::ros-info "Main" "I think object position is still valid")
+                            (planning-motion::call-Motion-Move-To-Point point-for-motion)
+                            (return-from check-for-valid-point))
                           )
-                      
+                         (roslisp::ros-info "Main" "Object position doesnt seem to be valid anymore, i will check again")
                     )
                    )
               )
