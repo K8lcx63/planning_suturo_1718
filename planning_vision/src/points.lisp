8d8ada3cb44b139d321eae3a182997e4da39dcab
Unknown <stoermer@uni-bremen.de>
2017-12-15 15:13:31 +0100

    Methode schoen
    Methode zum Punkte vergleichen in nicht haesslich

--- planning_vision/src/points.lisp
+++ planning_vision/src/points.lisp
@@ -10,19 +10,34 @@
 
 (defun check-points-is-equal (msg-one msg-two delta)
   "Maybe the ugliest function ever. Compares two points with delta."
-  (roslisp:with-fields (object) msg-one
-    (roslisp:with-fields (position) object
-      (roslisp:with-fields (point) position
-        (roslisp:with-fields (x y z) point
-          (let ((x1 x) (y1 y) (z1 z))
-            (roslisp:with-fields (object) msg-two
-               (roslisp:with-fields (position) object
-                 (roslisp:with-fields (point) position
-                   (roslisp:with-fields (x y z) point
-                     (let ((x2 x) (y2 y) (z2 z))
-                       (let ((dx (abs (- x1 x2))) (dy (abs (- y1 y2))) (dz (abs (- z1 z2))))
-                         (if (and (<= dx delta) (<= dy delta) (<= dz delta)) (print t)(print nil))
-                         ))))))))))))
+  (roslisp:with-fields ((x1 (geometry_msgs-msg:x geometry_msgs-msg:point)) 
+                        (y1 (geometry_msgs-msg:y geometry_msgs-msg:point))
+                        (z1 (geometry_msgs-msg:z geometry_msgs-msg:point)))
+                     (object_detection-msg:position (object_detection-srv:object msg-one))
+     (roslisp:with-fields ((x2 (geometry_msgs-msg:x geometry_msgs-msg:point)) 
+                           (y2 (geometry_msgs-msg:y geometry_msgs-msg:point))
+                           (z2 (geometry_msgs-msg:z geometry_msgs-msg:point)))
+                        (object_detection-msg:position (object_detection-srv:object msg-two))
+       (let (
+             (dx (abs (- x1 x2)))
+             (dy (abs (- y1 y2)))
+             (dz (abs (- z1 z2)))
+             )
+         (if (and
+              (<= dx delta)
+              (<= dy delta)
+              (<= dz delta)
+              )
+             (print t)
+             (print nil)
+             )
+         )
+       )
+    )
+  )
+    
+  
+  
   
 
 
