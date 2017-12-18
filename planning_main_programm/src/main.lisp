7927e1b9551bb065374e04e2cceb3ceea62f063f
Vanessa Hassouna <hassouna@uni-bremen.de>
2017-12-18 17:12:24 +0100

    search for object
    Pr2 is moving the head 6 times till he found an object

--- planning_main_programm/src/main.lisp
+++ planning_main_programm/src/main.lisp
@@ -1,29 +1,23 @@
 (in-package :planning-main-programm)
 
-(defvar *x* 1.0)
-(defvar *y* 0.0)
-(defvar *z* 0.5)
-
-
 
 (defun main ()
   "Main function - Executing and planning robot behaviour on the top level"
   (init-variables)
   (roslisp:with-ros-node ("planning_main")
-    (planning-move::move-Head *x* *y* *z*)
-    (planning-motion::call-Motion-Move-Arm)
-    (let ((point-for-motion
-            (let ((point-center-of-object
-                    (planning-vision::call-vision-point)))
-                 (planning-knowledge::ask-knowledge point-center-of-object))))
-         (planning-motion::call-Motion-Move-To-Point point-for-motion))))
+    (if (planning-move::find-Object 1.0 0.5)
+        (progn
+          (planning-motion::call-Motion-Move-Arm)
+          (let ((point-for-motion
+                  (let ((point-center-of-object
+                          (planning-vision::call-vision-point)))
+                    (planning-knowledge::ask-knowledge point-center-of-object))))
+            (planning-motion::call-Motion-Move-To-Point point-for-motion)))
+        (return-from main  (roslisp::ros-info "Main" "Sorry i couldnt find any object :c")))))
 
 
 (defun init-variables ()
   "initialize internal variables"
   (setf *x* 1.0)
-  (setf *y* 0.0)
   (setf *z* 0.5))
-
-
 
