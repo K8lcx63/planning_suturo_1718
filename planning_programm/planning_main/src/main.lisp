0854f4e17298f727d42953308812dbb732a4d95e
Vanessa <hassouna@uni-bremen.de>
2018-01-23 11:29:54 +0100

    Refactoring
    Die Namen der Funktionen wurden angepasst, sowie "schönheit" der Klamersetzung.

--- planning_programm/planning_main/src/main.lisp
+++ planning_programm/planning_main/src/main.lisp
@@ -6,7 +6,7 @@
   "Main function - Executing and planning robot behaviour on the top level"
   (roslisp:with-ros-node ("planning_main")
     (roslisp::ros-info "Main" "Robotlife seems hard, but lets do this")
-    (planning-motion::call-Motion-Move-Arm)
+    (planning-motion::call-Motion-Move-Arm-Homeposition)
     (if (planning-move::find-Object 1.0 0.5 "tolles Object")
         (progn
           (block check-for-valid-point 
@@ -16,25 +16,27 @@
                                (let ((point-center-of-object
                                        (progn
                                          (roslisp::ros-info "Main" "Calling Vision for center point of object")
-                                         (planning-vision::call-vision-point))))
+                                         (planning-vision::call-Vision-Point))))
                                  (progn
                                    (setf *point-center-of-object* point-center-of-object)
                                    (roslisp:ros-info "Main" "Asking knowledge where to poke")
-                                   (planning-knowledge::ask-knowledge point-center-of-object)))))
-                         (if (planning-vision::check-points-is-equal (planning-vision::call-vision-point) *point-center-of-object* 0.2)
+                                   (planning-knowledge::ask-Knowledge-For-Poke-Point point-center-of-object)))))
+                         (if (planning-vision::check-Points-Is-Equal
+                              (planning-vision::call-Vision-Point) *point-center-of-object* 0.2)
                              (progn
                                (roslisp::ros-info "Main" "I think object position is still valid")
-                               (planning-motion::call-Motion-Move-To-Point point-for-motion)
+                               (planning-motion::call-Motion-Move-Arm-To-Point point-for-motion)
                                (roslisp::ros-info "Main" "Object should be touched now")
                                (return-from check-for-valid-point))
                              (progn
                                (roslisp::ros-info "Main" "I suppose, that the Object moved. Will check if its still there.")
-                               (if (planning-move::find-Object 1.0 0.5 "tolles Object")()(progn
-                                                                                           (roslisp::ros-info "Main" "I cannot find the Object anymore.")
-                                                                                           (return-from check-for-valid-point))))
-                             )
+                               (if (planning-move::find-Object 1.0 0.5 "tolles Object")
+                                   ()
+                                   (progn
+                                     (roslisp::ros-info "Main" "I cannot find the Object anymore.")
+                                     (return-from check-for-valid-point)))))
                          (roslisp::ros-info "Main" "Object seems to be in my line of sight now, will look for exact position again now.")))))))
-    (return-from main  (roslisp::ros-info "Main" "Sorry i couldnt find any object :c"))))
+    (return-from main (roslisp::ros-info "Main" "Sorry i couldnt find any object :c"))))
 
 
 
--- planning_programm/planning_motion/src/motion-actions.lisp
+++ planning_programm/planning_motion/src/motion-actions.lisp
@@ -3,16 +3,16 @@
 (defun motion-Home ()
  "gibt die funktion mit true or false aus diese Dient erstmal als basic konstrukt und wird später noch genauer bearbeitet wenn die Funktioin drum herrum entstanden ist."
 (let ((result
-        (planning-motion::call-Motion-Move-Arm)))
+        (planning-motion::call-Motion-Move-Arm-Homeposition)))
   (roslisp:with-fields(successful) result (print successful))))
 
 (defun motion-To-Point (point-center-Of-object &optional (x 3))
  "gibt die funktion mit true or false aus diese Dient erstmal als basic konstrukt und wird später noch genauer bearbeitet wenn die Funktioin drum herrum entstanden ist.Optional kann man von dem linken zum rechten Arm wechseln 3=links 2=rechts"
 (let ((result
-        (planning-motion::call-Motion-Move-To-Point point-center-of-object x)))
+        (planning-motion::call-Motion-Move-Arm-To-Point point-center-of-object x)))
   (roslisp:with-fields(successful) result (return-from motion-To-Point successful))))
         
-(defun call-Motion-Move-Arm ()
+(defun call-Motion-Move-Arm-Homeposition()
   "Moves robot-arms into home position"
    (roslisp::ros-info "Motion" "getting into home position")
   (let ((actionclient 
@@ -30,7 +30,7 @@
 		
 
 
-(defun call-Motion-Move-To-Point (point-center-of-object &optional (x 3))
+(defun call-Motion-Move-Arm-To-Point (point-center-of-object &optional (x 3))
   "Moves choosen robot-arm (optional parameter) to the point-center-of-object (default right arm 3=right, 2=left)"
   (let ((actionclient 
     (actionlib:make-action-client "/moving" "motion_msgs/MovingCommandAction")))
