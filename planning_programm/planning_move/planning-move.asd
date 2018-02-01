b0487540b59ce242b70f77b4bdfb223c190ba36d
Unknown <hassouna@uni-bremen.de>
2018-02-01 18:46:57 +0100

    PokeItMoreWays wurde mit moveBase ersetzt
    -----------------------------
    Neue Funktionen:
    ----------------------------
    
    move-Base-To-Point (x y z w) "bewegt den Roboter anhand des map-frames"
    init-Robo-Moving () "Es wird ein paar mal rumgefahren (kleine distanzen) um die localization richtig zu starten"
    init-Action-Client-Base ()
    get-Action-Client-Base ()
    let-Robo-Try-To-Poke () "linker & rechter Arm wird probiert"
    try-To-Poke-Different-Location () "trying 4 different locations with 3 differents rotation"
    should-Robo-Use-Left-Or-Right-Arm () "findet herraus welcher arm schlauer wäre"
    
    ----------------------------------------
    Die launch Dateien wurden für die Simulation rdy gemacht.
    ----------------------------------------
    amcl_node.xml
    localization.launch
    map-2017-11-ease-demo.yaml
    pr2_nav_pcontroller2.launch
    map-2017-11-ease-demo-pgm

--- planning_programm/planning_move/planning-move.asd
+++ planning_programm/planning_move/planning-move.asd
@@ -10,7 +10,9 @@
                geometry_msgs-msg
                pr2_controllers_msgs-msg
 	       object_detection-srv
-	       motion_msgs-msg)
+	       motion_msgs-msg
+	       sensor_msgs-msg
+	       move_base_msgs-msg)
   :components
   ((:module "src"
     :components
--- planning_programm/planning_move/src/movement.lisp
+++ planning_programm/planning_move/src/movement.lisp
@@ -1,6 +1,9 @@
 (in-package :planning-move)
 
+
+(defvar *joint-states* 0)
 (defvar *beliefstateHead* 0)
+(defvar *action-client-base* nil)
 (defvar *headMovementList* '((0 -0.8)(1 -0.6)(2 -0.3)(3 0.0)(4 0.3)(5 0.6)(6 0.8)(7 0.6)(8 0.3)(9 0.0)(10 -0.3)(11 -0.6)))
 
 (defun move-Head (x y z)
@@ -17,23 +20,10 @@
 	(actionlib:make-action-goal actionclient target point-to-look-at)))
   (actionlib:call-goal actionclient actiongoal)))))
 
-(defun move-Base-To-Point (x y z w)
-  "Moving robot base via nav_pcontroller/move_base. X Y Z are treated as coordinates. W for Orientation."
-  (let ((actionclient 
-	(actionlib:make-action-client "nav_pcontroller/move_base" "move_base_msgs/MoveBaseAction")))
-  (loop until
-	(actionlib:wait-for-server actionclient))
-  (let ((pose-to-drive-to 
-	(cl-transforms-stamped:to-msg 
-	 (cl-transforms-stamped:make-pose-stamped "base_link" 0 
-                                            (cl-transforms:make-3d-vector x y z) (cl-tf:make-quaternion x y z w)))))
-  (let ((actiongoal 
-	(actionlib:make-action-goal actionclient target_pose pose-to-drive-to)))
-  (actionlib:call-goal actionclient actiongoal)))))
 
 
-(defun find-Object (x z objectString)
-  "Looking around from 0-2 and 0-(-2) to find an object, restarting at current Point if reused"
+(defun find-Object (x z)
+  "Looking around from 0-2 and 0-(-2) to find an object, restarting at current Point if reused OBJECTSTRING TODO WIEDER REIN"
   (block find-Object-Start 
     (loop for i from *beliefstateHead* to 24 do
       (let ((c (mod i 12)))
@@ -50,5 +40,109 @@
 
 
   
+(defun move-Base-To-Point (x y z angle)
+  "Moving robot base via nav_pcontroller/move_base. X Y Z are treated as coordinates. angle for Orientation."
+  (roslisp:ros-info (move-Base-To-Point)
+                    "before im moving ill make sure that my arms arent in the way!") 
+  (planning-motion::call-Motion-Move-Arm-Homeposition)
+  (get-action-client-base)
+  (let ((pose-to-drive-to 
+          (cl-transforms-stamped:to-msg 
+           (cl-transforms-stamped:make-pose-stamped "map" 10
+            (cl-transforms:make-3d-vector x y z)
+            (cl-transforms:axis-angle->quaternion
+             (cl-transforms:make-3d-vector 0 0 1)(/ (* angle pi) 180))))))
+    (roslisp:ros-info (move-Base-To-Point)
+                      "created quaternion, vector and pose-stamped") 
+    (let ((actiongoal 
+            (actionlib:make-action-goal *action-client-base* target_pose pose-to-drive-to)))
+      (roslisp:ros-info (move-Base-To-Point)
+                        "Sending actiongoal...") 
+      (actionlib:call-goal *action-client-base* actiongoal))))
 
   
+(defun move-Robo-Into-Homeposition ()
+   "moving robot into homeposition with frame map"
+(move-Base-To-Point 0.8 1 0 30))
+
+
+
+
+(defun init-Robo-Moving ()
+  "initialize all what's needed for the Robo to "
+  (planning-motion::call-Motion-Move-Arm-Homeposition)
+  (get-Action-Client-Base)
+  (roslisp:ros-info (init-Robo-Moving)
+                    "robo is starting to move...")
+  (let ((positions-x (make-array '(4)  
+                                 :initial-contents '(0.0 0.1 0.2 0.0)))) 
+    (loop for i across positions-x do
+        (move-Base-To-Point i 0 0 0)))
+  (roslisp:ros-info (init-Robo-Moving)
+                    "robo is ready..."))
+
+   
+
+(defun init-Action-Client-Base ()
+  "creating move_base actionclient"
+  (setf *action-client-base* 
+        (actionlib:make-action-client
+         "nav_pcontroller/move_base"
+         "move_base_msgs/MoveBaseAction"))
+  (roslisp:ros-info (init-Action-Client-Base)
+                    "Waiting for move_base action server...")
+  (loop until
+        (actionlib:wait-for-server *action-client-base*))
+  (roslisp:ros-info (init-Action-Client-Base) 
+                    "move_base action client created."))
+
+
+(defun get-Action-Client-Base ()
+  (when (null *action-client-base*)
+    (init-Action-Client-Base))
+  *action-client-base*)
+
+
+
+(defun save-joint-states (msg)
+"Callback to save one sensor-msgs/JointState"
+  (setf *joint-states* msg))
+
+(defun get-joint-states ()
+"gets exactly one sensor_msgs/JointState message"
+  (progn
+    (let
+        ((subsc
+           (roslisp:subscribe "/joint_states" "sensor_msgs/JointState" #'save-joint-states :max-queue-length 1)))
+      (progn
+        (sleep 1)
+        (roslisp:unsubscribe subsc)))
+    (return-from get-joint-states *joint-states*)))
+
+(defun is-gripper-filled (side)
+  "Checks if Gripper is filled after gripping something. Side is either left or right as string"
+  (if
+   (string= side "left")
+   (progn
+      (get-joint-states)
+      (roslisp:with-fields
+          ((Name (sensor_msgs-msg:Name))
+           (Position (sensor_msgs-msg:Position))) planning-move::*joint-states* 
+        (loop for a across Name 
+              for b across Position do
+                (if (string= a "l_gripper_joint")
+                    (if (and
+                         (>= b 0.004)
+                         (<= b 0.08))(return-from is-gripper-filled T)(print b))))))
+   (progn
+      (get-joint-states)
+      (roslisp:with-fields
+          ((Name (sensor_msgs-msg:Name))
+           (Position (sensor_msgs-msg:Position))) planning-move::*joint-states* 
+        (loop for a across Name 
+              for b across Position do
+                (if (string= a "r_gripper_joint")
+                    (if (and
+                         (>= b 0.004)
+                         (<= b 0.08))(return-from is-gripper-filled T)(print b))))))))
+
